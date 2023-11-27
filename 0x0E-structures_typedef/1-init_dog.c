62ad5cd2ca3c2f858eac390e7f651fbe6f6375b9
John10tef <yohannes.teferi10@gmail.com>
2023-11-27 11:52:22 +0000

    A dog will teach you unconditional love. If you can have that in your life, things won't be too bad

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include "dog.h"
+
 /**
 * init_dog - initializes a variable of type struct dog
 * @d: pointer to struct dog to initialize
@@ -7,6 +8,7 @@
 * @age: age to initialize
 * @owner: owner to initialize
 */
+
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 if (d == NULL)
