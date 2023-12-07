128bfeebbc0f7a896ef527d743763aa6a08e76ef
John10tef <yohannes.teferi10@gmail.com>
2023-12-07 20:37:36 +0000

    hello

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -2,13 +2,17 @@ global main
 extern printf
 
 section .data
-    format db "Hello, Holberton", 0
+	format db "Hello, Holberton", 0
+	newline db 10, 0
 
 section .text
 
 main:
-    mov edi, format
-    xor eax, eax
-    call printf
-
-    ret
+	mov edi, format
+	xor eax, eax
+	call printf
+	
+	mov edi, newline
+	xor eax, eax
+	call printf
+	ret
