f446e4770d59d655bb2b6fdbdc821e97faa93ef7
John10tef <yohannes.teferi10@gmail.com>
2023-12-07 19:54:08 +0000

    first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void print_message_before_main(void)
+{
+    printf("You're beat! and yet, you must allow,\n");
+    printf("I bore my house upon my back!\n");
+}
+
+int main(void)
+{
+    print_message_before_main();
+
+    /* Rest of your code goes here... */
+
+    return 0;
+}
