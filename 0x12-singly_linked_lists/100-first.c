2f1af5585775b5037fe7a9ba7b97f1e60c6edfa6
John10tef <yohannes.teferi10@gmail.com>
2023-12-07 19:58:46 +0000

    100 task

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
 
-void print_message_before_main(void)
+void first(void) __attribute__ ((constructor));
+
+/**
+ * first - Prints a sentence before the main
+ * function is executed.
+ */
+void first(void)
 {
-    printf("You're beat! and yet, you must allow,\n");
-    printf("I bore my house upon my back!\n");
-}
-
-int main(void)
-{
-    print_message_before_main();
-
-    /* Rest of your code goes here... */
-
-    return 0;
+	printf("You're beat! and yet, you must allow,\n"
+	       "I bore my house upon my back!\n");
 }
