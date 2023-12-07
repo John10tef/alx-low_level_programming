247450b03c48b9558c23ae73c71a29a8c0f6902b
John10tef <yohannes.teferi10@gmail.com>
2023-12-07 18:41:12 +0000

    task 2

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 
-/**
-* print_list - Prints all the elements of a list_t list.
-* @h: A pointer to the head of the list_t list.
-*
-* Return: The number of nodes in the list_t list.
+/** 
+ * print_list - Prints all the elements of a list_t list.
+ * @h: A pointer to the head of the list_t list.
+ *
+ * Return: The number of nodes in the list_t list.
 */
 
 size_t print_list(const list_t *h)
