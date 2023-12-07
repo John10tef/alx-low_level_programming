c50d8b319c6e52d4e37cc60ce2a617c8937a3b10
John10tef <yohannes.teferi10@gmail.com>
2023-12-07 19:11:44 +0000

    mandatory done

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -7,25 +7,26 @@
  *
  * Return: Address of the new element, or NULL if it failed.
  */
-
 list_t *add_node(list_t **head, const char *str)
 {
-	if (str == NULL)
-		return (NULL);
+	list_t *new_node;
 
-	list_t *new_node = malloc(sizeof(list_t));
+	if (str == NULL)
+		return NULL;
+
+	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
-		return (NULL);
+		return NULL;
 
 	new_node->str = strdup(str);
 	if (new_node->str == NULL)
 	{
 		free(new_node);
-		return (NULL);
+		return NULL;
 	}
 
 	new_node->next = *head;
 	*head = new_node;
 
-	return (new_node);
+	return new_node;
 }
--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -9,18 +9,21 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	if (str == NULL)
-		return (NULL);
+	list_t *new_node;
+	list_t *current;
 
-	list_t *new_node = malloc(sizeof(list_t));
+	if (str == NULL)
+		return NULL;
+
+	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
-		return (NULL);
+		return NULL;
 
 	new_node->str = strdup(str);
 	if (new_node->str == NULL)
 	{
 		free(new_node);
-		return (NULL);
+		return NULL;
 	}
 
 	new_node->next = NULL;
@@ -31,11 +34,11 @@ list_t *add_node_end(list_t **head, const char *str)
 	}
 	else
 	{
-		list_t *current = *head;
+		current = *head;
 		while (current->next != NULL)
 			current = current->next;
 		current->next = new_node;
 	}
 
-	return (new_node);
+	return new_node;
 }
